950b2d18edb060c792f387623ef4cbb3e941df94
eempc <evc484@gmail.com>
2019-02-23 16:38:13 +0000

    ending this inline method, switching to grid method

--- index.js
+++ index.js
@@ -2,20 +2,44 @@
 
 const container = document.querySelector("#container");
 
-for (var i = 0; i < 16; i++) {
-    var node = document.createElement("div");
-    node.classList.add("column0-label");
-    node.appendChild(document.createTextNode("row: " + i));
+function generateGrid(x) {
+    deleteGrid();
 
-    for (var j = 0; j < 16; j++) {
-        var node2 = document.createElement("div");
-        node2.classList.add("grid-cell");
-        node2.appendChild(document.createTextNode(j));
-        node.appendChild(node2);
-    }
-
-    container.appendChild(node);
+    for (var i = 0; i < x; i++) {
+        var node = document.createElement("div");
+        node.classList.add("column0-label");
+        var numString = (i < 10) ? "0" + i: i;
+        node.appendChild(document.createTextNode("row: " + numString));
     
+        for (var j = 0; j < x; j++) {
+            var node2 = document.createElement("div");
+            node2.classList.add("grid-cell");
+            node2.appendChild(document.createTextNode(j));
+            node.appendChild(node2);
+        }
+    
+        container.appendChild(node); 
+    }
 }
 
+function deleteGrid() {
+    var currentGrid = document.querySelectorAll(".grid-cell");
+    currentGrid.forEach(e => e.parentNode.removeChild(e));
 
+    var currentLabels = document.querySelectorAll(".column0-label");
+    currentLabels.forEach(e => e.parentNode.removeChild(e));
+}
+
+generateGrid(16);
+
+const resetButton = document.querySelector("#reset-button");
+resetButton.addEventListener("click", resetGrid)
+
+function resetGrid (){
+    var size = prompt("enter size", 5);
+    generateGrid(size);
+}
+
+class formatDigits {
+
+}
